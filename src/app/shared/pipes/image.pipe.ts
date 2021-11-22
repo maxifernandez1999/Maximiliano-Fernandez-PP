d7d6747819c89a9a7ec33346b7a571ca7a0abfd9
maxifernandez1999 <fmaximiliano443@gmail.com>
2021-11-22 15:50:42 -0300

    agregados de pipes

--- src/app/shared/pipes/image.pipe.ts
+++ src/app/shared/pipes/image.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ImagePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(imagePath: string): string {
+    return '.';
   }
 
 }
